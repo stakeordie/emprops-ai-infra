b6c2d92a22520a2a1d49c21557e40a36ff183392
Sandy Corsillo <the_dusky@icloud.com>
2024-02-06 02:08:34 -0500

    models 22

--- services/AUTOMATIC1111/entrypoint.sh
+++ services/AUTOMATIC1111/entrypoint.sh
@@ -24,8 +24,13 @@ if [ ! -f /data/config/auto/styles.csv ]; then
   touch /data/config/auto/styles.csv
 fi
 
+echo "Installing pm2..."
+apt-get install -y ca-certificates curl gnupg
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_16.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
 apt-get update
-apt-get install -y nodejs
+apt-get install nodejs -y
 npm install -g npm@9.8.0
 npm install -g pm2@latest
 
