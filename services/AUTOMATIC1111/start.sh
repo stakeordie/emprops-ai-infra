1c7f44f49a92d2d4f4462cd4d08df5e4a1b722fe
Sandy Corsillo <the_dusky@icloud.com>
2024-02-10 01:08:44 -0500

    pm2 load models 1 at a time 21

--- services/AUTOMATIC1111/start.sh
+++ services/AUTOMATIC1111/start.sh
@@ -5,12 +5,12 @@ set -Eeuo pipefail
 mkdir ${ROOT}/models/Stable-diffusion && cd ${ROOT}/models/Stable-diffusion
 wget --no-verbose --show-progress --progress=bar:force:noscroll https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned.safetensors
 wget --no-verbose --show-progress --progress=bar:force:noscroll https://huggingface.co/stabilityai/stable-diffusion-2-1/resolve/main/v2-1_768-ema-pruned.safetensors
-wget --no-verbose --show-progress --progress=dot:giga https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors
-wget --no-verbose --show-progress --progress=dot:giga https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0.safetensors
+wget --no-verbose --show-progress --progress=bar:force:noscroll https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors
+wget --no-verbose --show-progress --progress=bar:force:noscroll https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0.safetensors
 
 cd ${ROOT}
 
-pm2 start --name webui "python -u webui.py --opt-sdp-no-mem-attention --api --nowebui --port 3130 --medvram --no-half-vae"
+pm2 start --name webui "python -u webui.py --opt-sdp-no-mem-attention --api --port 3130 --medvram --no-half-vae"
 
 service nginx start
 
