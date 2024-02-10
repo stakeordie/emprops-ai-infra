e7dbd2945f4aa08efe7637676c1ac32af84357a0
Sandy Corsillo <the_dusky@icloud.com>
2024-02-09 21:28:10 -0500

    pm2 load models 1 at a time 3

--- services/AUTOMATIC1111/start.sh
+++ services/AUTOMATIC1111/start.sh
@@ -11,6 +11,17 @@ IFS=, read -r -a models <<<"${MODELS}"
 
 # Array to parameter list
 echo "Loading models: ${MODELS}"
-for model in "${models[@]}"; do echo $model && python loader.py -m $model; done
+
+echo $number  # Output: 25
+pm2 logs --format | grep auto | while read line
+do
+    id=$(echo "$line" | grep -oP '(?<=id=)\d+')
+    echo "$line" | grep "message=Model loaded in"
+    if [ $? = 0 ]
+    then
+      for model in "${models[@]}"; do echo $model && python loader.py -m $model; done
+      break;
+    fi
+done
 
 sleep infinity
