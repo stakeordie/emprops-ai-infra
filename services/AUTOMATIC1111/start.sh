23636683084a2f087a2c39e8aa5f35bdb385ffbb
Sandy Corsillo <the_dusky@icloud.com>
2024-02-09 23:02:00 -0500

    pm2 load models 1 at a time 16

--- services/AUTOMATIC1111/start.sh
+++ services/AUTOMATIC1111/start.sh
@@ -3,10 +3,14 @@
 set -Eeuo pipefail
 
 mkdir ${ROOT}/models/Stable-diffusion && cd ${ROOT}/models/Stable-diffusion
-wget https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned.ckpt
-wget https://huggingface.co/stabilityai/stable-diffusion-2-1/resolve/main/v2-1_768-ema-pruned.ckpt
-wget https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors
-wget https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0.safetensors
+wget \
+  --no-verbose --show-progress \
+  --progress=dot:mega \
+  url
+wget --no-verbose --show-progress --progress=dot:giga https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned.ckpt
+wget --no-verbose --show-progress --progress=dot:giga https://huggingface.co/stabilityai/stable-diffusion-2-1/resolve/main/v2-1_768-ema-pruned.ckpt
+wget --no-verbose --show-progress --progress=dot:giga https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors
+wget --no-verbose --show-progress --progress=dot:giga https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0.safetensors
 
 cd ${ROOT}
 
