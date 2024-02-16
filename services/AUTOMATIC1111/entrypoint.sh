11b0962fad9b2042667855f321318512fc45d2c4
Sandy Corsillo <the_dusky@icloud.com>
2024-02-16 14:27:59 -0500

    remove ENV variables 5

--- services/AUTOMATIC1111/entrypoint.sh
+++ services/AUTOMATIC1111/entrypoint.sh
@@ -6,10 +6,11 @@ set -Eeuo pipefail
 mkdir -p /data/config/auto/scripts/
 # mount scripts individually
 
-while getopts "r:" flag > /dev/null 2>&1
+while getopts "r:m:" flag > /dev/null 2>&1
 do
     case ${flag} in
         r) BOOT="${OPTARG}" ;;
+        m) MODELS="${OPTARG}" ;;
         *) break;; 
     esac
 done
@@ -107,3 +108,32 @@ if [ -f "/data/config/auto/startup.sh" ]; then
   . /data/config/auto/startup.sh
   popd
 fi
+
+mkdir ${BOOT}/models/Stable-diffusion && cd ${BOOT}/models/Stable-diffusion
+wget --no-verbose --show-progress --progress=bar:force:noscroll https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned.safetensors
+wget --no-verbose --show-progress --progress=bar:force:noscroll https://huggingface.co/stabilityai/stable-diffusion-2-1/resolve/main/v2-1_768-ema-pruned.safetensors
+wget --no-verbose --show-progress --progress=bar:force:noscroll https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors
+wget --no-verbose --show-progress --progress=bar:force:noscroll https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0.safetensors
+wget --no-verbose --show-progress --progress=bar:force:noscroll "https://civitai.com/api/download/models/288982?type=Model&format=SafeTensor&size=full&fp=fp16" -O juggernautXL_v8Rundiffusion.safetensors
+cd ${BOOT}
+
+pm2 start --name webui "python -u webui.py --opt-sdp-no-mem-attention --api --port 3130 --medvram --no-half-vae"
+
+service nginx start
+
+# Comma separated string to array
+IFS=, read -r -a models <<<"${MODELS}"
+
+# Array to parameter list
+echo "WAITING TO START UP BEFORE LOADING MODELS..."
+
+sleep 75
+
+# Array to parameter list
+echo "Loading models: ${MODELS}"
+
+for model in "${models[@]}"; do echo $model && python /docker/loader.py -m $model; done
+
+echo "~~READY~~"
+
+sleep infinity
