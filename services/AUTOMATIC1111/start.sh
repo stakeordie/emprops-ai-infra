28626aecc9e44cfbafd1ebb0b5bbbd7cb4f678e7
Sandy Corsillo <the_dusky@icloud.com>
2024-02-16 09:47:52 -0500

    wait a bit longer

--- services/AUTOMATIC1111/start.sh
+++ services/AUTOMATIC1111/start.sh
@@ -20,7 +20,7 @@ IFS=, read -r -a models <<<"${MODELS}"
 # Array to parameter list
 echo "Loading models: ${MODELS}"
 
-sleep 50
+sleep 75
 
 for model in "${models[@]}"; do echo $model && python /docker/loader.py -m $model; done
 
