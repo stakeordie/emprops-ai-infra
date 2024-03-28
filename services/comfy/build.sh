93f011e55ced4f503a154fa453774f9a462c84b6
Sandy Corsillo <the_dusky@icloud.com>
2024-03-28 15:59:14 -0400

    fix model loader issue

--- services/comfy/build.sh
+++ services/comfy/build.sh
@@ -9,7 +9,7 @@ ls -lha $ROOT
 ls -lha / 
 
 echo "Installing pm2..."
-apt-get install -y ca-certificates curl gnupg
+apt-get install -y ca-certificates curl gnupg ufw
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_16.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
@@ -30,6 +30,7 @@ ssh-keyscan github.com > ~/.ssh/githubKey
 ssh-keygen -lf ~/.ssh/githubKey
 cat ~/.ssh/githubKey >> ~/.ssh/known_hosts
 git clone -b comfy-node git@github.com:stakeordie/emprops-nginx-conf.git /etc/nginx
+apt install -y nvtop
 service nginx start
 
 # Comma separated string to array
